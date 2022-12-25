cc8b41895b8c2936c60b6bc1b6b63046274f7651
ImYourGod <carlos2003cjj@gmail.com>
2022-12-25 15:24:33 +0100

    Update [Enemy Blood Spawn]

--- LaboratoryScape/Assets/Members/Carlos/Imports/AssetStore/Kinemation/FPSFramework/Weapon.cs
+++ LaboratoryScape/Assets/Members/Carlos/Imports/AssetStore/Kinemation/FPSFramework/Weapon.cs
@@ -22,9 +22,16 @@ namespace Demo.Scripts.Runtime
         /////////////////////////////////////////////
         /////////////////////////////////////////////
         
-        public Transform muzzle;
-        public LayerMask enemyColider;
-        public float impactForce;
+        [SerializeField] private Transform muzzle;
+        [SerializeField] private LayerMask enemyColider;
+        [SerializeField] private float impactForce;
+
+        [Header("--- BLOOD ---")]
+        public Light DirLight;
+        public GameObject BloodAttach;
+        public GameObject[] BloodFX;
+        public Vector3 direction;
+        int effectIdx;
         
         /////////////////////////////////////////////
         /////////////////////////////////////////////
@@ -52,6 +59,33 @@ namespace Demo.Scripts.Runtime
                 {
                     hit.collider.GetComponentInParent<EnemyDespossess>().EnemyDie();
                     hit.collider.GetComponent<Rigidbody>().AddForce(-hit.normal * impactForce);
+                    
+                    /////////////////////////////////////////////////////////////////////////////
+                    /////////////////////////////////////////////////////////////////////////////
+                    
+                    // var randRotation = new Vector3(0, Random.value * 360f, 0);
+                    // var dir = CalculateAngle(Vector3.forward, hit.normal);
+                    float angle = Mathf.Atan2(hit.normal.x, hit.normal.z) * Mathf.Rad2Deg + 180;
+
+                    //var effectIdx = Random.Range(0, BloodFX.Length);
+                    if (effectIdx == BloodFX.Length) effectIdx = 0;
+
+                    var instance = Instantiate(BloodFX[effectIdx], hit.point, Quaternion.Euler(0, angle + 90, 0));
+                    effectIdx++;
+
+                    var settings = instance.GetComponent<BFX_BloodSettings>();
+                    //settings.FreezeDecalDisappearance = InfiniteDecal;
+                    settings.LightIntensityMultiplier = DirLight.intensity;
+
+                    var attachBloodInstance = Instantiate(BloodAttach);
+                    var bloodT = attachBloodInstance.transform;
+                    bloodT.position = hit.point;
+                    bloodT.localRotation = Quaternion.identity;
+                    bloodT.localScale = Vector3.one * Random.Range(0.75f, 1.2f);
+                    bloodT.LookAt(hit.point + hit.normal, direction);
+                    bloodT.Rotate(90, 0, 0);
+                    bloodT.transform.parent = hit.transform;
+                    //Destroy(attachBloodInstance, 20);
                 }
             }
 
