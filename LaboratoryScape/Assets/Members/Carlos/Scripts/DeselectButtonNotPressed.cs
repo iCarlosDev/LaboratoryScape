234efd4016876b4090ad409e491762097cabc74a
lmYourGod <73672853+lmYourGod@users.noreply.github.com>
2023-01-20 15:49:44 +0100

    Update [Menu Buttons Fade FIXED]

--- LaboratoryScape/Assets/Members/Carlos/Scripts/DeselectButtonNotPressed.cs
+++ LaboratoryScape/Assets/Members/Carlos/Scripts/DeselectButtonNotPressed.cs
@@ -8,16 +8,14 @@ using UnityEngine.UI;
 public class DeselectButtonNotPressed : MonoBehaviour
 {
     [SerializeField] private EventSystem eventSystem;
-    [SerializeField] private Button _button;
 
     private void Awake()
     {
         eventSystem = FindObjectOfType<EventSystem>();
-        _button = GetComponent<Button>();
     }
 
     public void RemoveCurrentSelected()
     {
-        _button.OnDeselect(eventSystem.MDummyData);
+        eventSystem.SetSelectedGameObject(null);
     }
 }
