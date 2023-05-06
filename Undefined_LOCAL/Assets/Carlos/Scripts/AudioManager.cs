4a1bdc6127413bcc7e94ea2df1769b4dd5f4ccfa
lmYourGod <73672853+lmYourGod@users.noreply.github.com>
2023-05-06 12:57:31 +0200

    [Sounds Started]

--- Undefined_LOCAL/Assets/Carlos/Scripts/AudioManager.cs
+++ Undefined_LOCAL/Assets/Carlos/Scripts/AudioManager.cs
@@ -1,18 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Sound[] sounds;
+    public static AudioManager instance;
+
+    private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         
+        DontDestroyOnLoad(gameObject);
+        
+        foreach (Sound s in sounds)
+        {
+           s.source = gameObject.AddComponent<AudioSource>();
+           s.source.clip = s.clip;
+
+           s.source.volume = s.volume;
+           s.source.pitch = s.pitch;
+           s.source.loop = s.loop;
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Play("MainTheme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void PlayOneShot(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.PlayOneShot(s.source.clip);
+    }
+    
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
     }
 }
