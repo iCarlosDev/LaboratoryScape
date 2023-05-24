3800066cf795631340cf07617d90222d9ecfab4c
Mary Estarli <m.estarli@santmarc.com>
2023-05-24 18:09:03 +0200

    Skip intro secuence

---
+++ Undefined_LOCAL/Assets/Mary/Scripts/SkipIntro.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipIntro : MonoBehaviour
+{
+    [SerializeField] private GameObject enableButtonUI;
+    private bool skipIntro = false;
+
+    private void Awake()
+    {
+        enableButtonUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey && !enableButtonUI.active)
+        {
+           enableButtonUI.SetActive(true);
+           StartCoroutine(SkipIntroCoroutine());
+        }
+        if (Input.anyKey && skipIntro)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+    IEnumerator SkipIntroCoroutine()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(0.02f);
+        skipIntro = true;
+    }
+}
